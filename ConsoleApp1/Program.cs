e5ecce7a03acb4cbe9b4848fe9b336de7eb939fb
Тарас Русакович <tarus01@mail.ru>
2021-12-17 15:11:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ipr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>();
+            st.Notify += DisplayMessage;
+
+            Console.WriteLine("Add elements to Stack and press Enter to finish" +
+                              "\npress 1 to add" +
+                              "\npress 2 to remove");
+
+            bool temp = true;
+            int k;
+            while (temp == true)
+            {
+                try
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+
+                        case ConsoleKey.D1:
+                            {
+                                Console.Write("member:");
+                                st.Add(int.Parse(Console.ReadLine()));
+                                break;
+                            }
+
+                        case ConsoleKey.D2:
+                            {
+                                st.Remove(out k);
+                                Console.WriteLine($"     Removed member: {k}");
+                                break;
+                            }
+
+                        case ConsoleKey.Enter:
+                            {
+                                temp = false;
+                                break;
+                            }
+
+                        default:
+                            {
+                                Console.WriteLine("Please, choose what you want to do");
+                                break;
+                            }
+                    }
+                }
+
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("try again");
+                    Console.ResetColor();
+                    continue;
+                }
+            }
+                
+            k = st.Peek();
+            Console.WriteLine($"the first memeber {k}");
+
+            st.Clear();
+            Console.WriteLine($"number of objects in stack {st.Count}");
+
+            
+
+            Console.ReadKey();
+        }
+
+        private static void DisplayMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+    }
+    
+}
---
+++ ConsoleApp1/Stack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipr1
+{
+    public class Stack<T>
+    {
+        private List<T> Stack_Col;
+
+        public delegate void CollHandler(string message);                   //event declaration
+        public event CollHandler Notify;
+
+
+        public Stack()
+        {
+            Stack_Col = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            Stack_Col.Add(item);
+            Notify?.Invoke($"item {item} has been added");
+        }
+
+        public int Count
+        {
+            get
+            {
+                return Stack_Col.Count;
+            }
+        }
+
+
+        public bool Remove(out T item)
+        {
+            bool result = false;
+            if(Stack_Col.Count!= 0)
+            {
+                item = Stack_Col[Stack_Col.Count-1];
+                Notify?.Invoke($"object {item} has been removed");
+                Stack_Col.RemoveAt(Stack_Col.Count-1);
+                result = true;
+            }
+            else 
+            { 
+                item = default(T); 
+                Notify?.Invoke("It's unable to remove - Stack is empty"); 
+            }
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (Stack_Col.Count != 0) { return Stack_Col[Stack_Col.Count - 1]; }
+
+            else { return default(T); }
+        }
+
+        public void Clear()
+        {
+            Stack_Col.Clear();
+            Notify?.Invoke("Stack has been freed");
+        }
+
+    }
+}
